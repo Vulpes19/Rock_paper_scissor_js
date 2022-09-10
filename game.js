3eea5a0393d77075325f1be421464b1fcf3c6b18
Vulpes19 <baioumyaymann@gmail.com>
2022-09-10 20:47:38 +0100

    now the moves left number doesn't display when the game ends

--- game.js
+++ game.js
@@ -7,7 +7,8 @@ let player_moves = 3;
 let computer_moves = 3;
 let computer_choice = null;
 const player_options = [rock_button, paper_button, scissor_button];
-const computer_options = ['rock', 'paper', 'scissor'];
+const computer_options = ["rock", "paper", "scissor"];
+const   moves_left = document.querySelector(".moves");
 
 function ft_game()
 {
@@ -19,20 +20,19 @@ function ft_play()
 {
     console.log(player_moves, computer_moves);
     if (player_moves == 0 && computer_moves == 0)
-            ft_game_over(player_options, moves_left);
+        ft_game_over(player_options, moves_left);
     player_options.forEach(ft_handle_events);
 }
 
 
 function ft_handle_events(option)
 {
-    option.addEventListener('click', (event) => {
-        const   moves_left = document.querySelector('.moves');
+    option.addEventListener("click", (event) => {
         if (player_moves == 0)
             ft_game_over(player_options, moves_left);
-        player_moves--;
         computer_moves--;
-        moves_left.innerText = 'Moves Left ${player_moves}';
+        player_moves--;
+        moves_left.innerText = "Moves Left " + player_moves;
         const choice = event.target.innerText;
         const   computer_choice = computer_options[Math.floor(Math.random() * 3)];
         ft_if_gamesfinished(choice, computer_choice);
@@ -54,54 +54,54 @@ function ft_handle_events(option)
     
 function ft_if_gamesfinished(player, computer)
 {
-    const result = document.querySelector('.result');
-    const pl_score_board = document.querySelector('.pl_score');
-    const cp_score_board = document.querySelector('.cp_score');
+    const result = document.querySelector(".result");
+    const pl_score_board = document.querySelector(".pl_score");
+    const cp_score_board = document.querySelector(".cp_score");
     player = player.toLowerCase();
     computer = computer.toLowerCase();
     if (player == computer)
-        result.textContent = 'DRAW!';
-        else if (player == 'rock')
+        result.textContent = "DRAW!";
+        else if (player == "rock")
     {
-        if (computer == 'paper')
+        if (computer == "paper")
         {
-            result.textContent = 'Computer won !';
+            result.textContent = "Computer won !";
             computer_score++;
             cp_score_board.textContent = computer_score;
         }
         else
         {
-            result.textContent = 'You won !';
+            result.textContent = "You won !";
             player_score++;
             pl_score_board.textContent = player_score;
         }
     }
-    else if (player == 'paper')
+    else if (player == "paper")
     {
-        if (computer == 'scissor')
+        if (computer == "scissor")
         {
-            result.textContent = 'Computer won !';
+            result.textContent = "Computer won !";
             computer_score++;
             cp_score_board.textContent = computer_score;
         }
         else
         {
-            result.textContent = 'You won !';
+            result.textContent = "You won !";
             player_score++;
             pl_score_board.textContent = player_score;
         }
     }
-    else if (player == 'scissor')
+    else if (player == "scissor")
     {
-        if (computer == 'rock')
+        if (computer == "rock")
         {
-            result.textContent = 'Computer won !';
+            result.textContent = "Computer won !";
             computer_score++;
             cp_score_board.textContent = computer_score;
         }
         else
         {
-            result.textContent = 'You won !';
+            result.textContent = "You won !";
             player_score++;
             pl_score_board.textContent = player_score;
         }
@@ -110,32 +110,33 @@ function ft_if_gamesfinished(player, computer)
 
 function ft_game_over(player_options, moves_left)
 {
-    const move = document.querySelector('.move');
-    const result = document.querySelector('.result');
-    const reload_button = document.querySelector('.refresh');
+    const move = document.querySelector(".move");
+    const result = document.querySelector(".result");
+    const reload_button = document.querySelector(".refresh");
     
-    player_options.forEach( (option) => {option.style.display = 'none';});
-    move.style.display = 'none';
+    moves_left.style.display = "none";
+    player_options.forEach( (option) => {option.style.display = "none";});
+    move.style.display = "none";
     if (player_score > computer_score)
     {
-        result.InnerText = 'You Won :D';
-        result.style.FontSize = '2rem';
-        result.style.color = '#8E7DBE';
+        result.InnerText = "You Won :D";
+        result.style.FontSize = "2rem";
+        result.style.color = "#8E7DBE";
     }
     else if (player_score < computer_score)
     {
-        result.InnerText = 'You Lost D:';
-        result.style.FontSize = '2rem';
-        result.style.color = '#8E7DBE';
+        result.InnerText = "You Lost D:";
+        result.style.FontSize = "2rem";
+        result.style.color = "#8E7DBE";
     }
     else
     {
-        result.InnerText = 'DRAW';
-        result.style.FontSize = '2rem';
-        result.style.color = '#8E7DBE';
+        result.InnerText = "DRAW";
+        result.style.FontSize = "2rem";
+        result.style.color = "#8E7DBE";
     }
-    reload_button.style.InnerText = 'Restart';
-    reload_button.addEventListener('click', function (){window.location.reload();});
+    reload_button.style.InnerText = "Restart";
+    reload_button.addEventListener("click", function (){window.location.reload();});
 }
 
 ft_game();
